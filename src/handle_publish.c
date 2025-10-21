c641f1d45af17b1729c30959fef2c7aabd881afd
Roger A. Light <roger@atchoo.org>
2025-10-21 17:36:12 +0100

    Refactor handling of accepted publish messages

--- src/handle_publish.c
+++ src/handle_publish.c
@@ -32,22 +32,181 @@ Contributors:
 #include "util_mosq.h"
 
 
+static int process_bad_message(struct mosquitto *context, struct mosquitto__base_msg *base_msg, uint8_t reason_code)
+{
+	int rc = MOSQ_ERR_UNKNOWN;
+	if(base_msg){
+		switch(base_msg->data.qos){
+			case 0:
+				rc = MOSQ_ERR_SUCCESS;
+				break;
+			case 1:
+				if(context){
+					rc = send__puback(context, base_msg->data.source_mid, reason_code, NULL);
+				}else{
+					rc = MOSQ_ERR_SUCCESS;
+				}
+				break;
+			case 2:
+				if(context){
+					rc = send__pubrec(context, base_msg->data.source_mid, reason_code, NULL);
+				}else{
+					rc = MOSQ_ERR_SUCCESS;
+				}
+				break;
+		}
+		db__msg_store_free(base_msg);
+	}
+	if(context && db.config->max_queued_messages > 0 && context->out_packet_count >= db.config->max_queued_messages){
+		rc = MQTT_RC_QUOTA_EXCEEDED;
+	}
+	return rc;
+}
+
+
+int handle__accepted_publish(struct mosquitto *context, struct mosquitto__base_msg *base_msg, uint16_t mid, int dup, uint32_t *message_expiry_interval)
+{
+	int rc;
+	int rc2;
+	struct mosquitto__base_msg *stored = NULL;
+	struct mosquitto__client_msg *cmsg_stored = NULL;
+
+	{
+		rc = plugin__handle_message_in(context, &base_msg->data);
+		if(rc == MOSQ_ERR_ACL_DENIED){
+			log__printf(NULL, MOSQ_LOG_DEBUG,
+					"Denied PUBLISH from %s (d%d, q%d, r%d, m%d, '%s', ... (%ld bytes))",
+					context->id, dup, base_msg->data.qos, base_msg->data.retain, base_msg->data.source_mid, base_msg->data.topic,
+					(long)base_msg->data.payloadlen);
+
+			return process_bad_message(context, base_msg, MQTT_RC_NOT_AUTHORIZED);
+		}else if(rc == MOSQ_ERR_QUOTA_EXCEEDED){
+			log__printf(NULL, MOSQ_LOG_DEBUG,
+					"Rejected PUBLISH from %s, quota exceeded.", context->id);
+
+			return process_bad_message(context, base_msg, MQTT_RC_QUOTA_EXCEEDED);
+		}else if(rc != MOSQ_ERR_SUCCESS){
+			db__msg_store_free(base_msg);
+			return rc;
+		}
+	}
+
+	if(base_msg->data.qos > 0){
+		db__message_store_find(context, base_msg->data.source_mid, &cmsg_stored);
+	}
+
+	if(cmsg_stored && base_msg->data.source_mid != 0 &&
+			(cmsg_stored->base_msg->data.qos != base_msg->data.qos
+			|| cmsg_stored->base_msg->data.payloadlen != base_msg->data.payloadlen
+			|| strcmp(cmsg_stored->base_msg->data.topic, base_msg->data.topic)
+			|| memcmp(cmsg_stored->base_msg->data.payload, base_msg->data.payload, base_msg->data.payloadlen))){
+
+		log__printf(NULL, MOSQ_LOG_WARNING, "Reused message ID %u from %s detected. Clearing from storage.", base_msg->data.source_mid, context->id);
+		db__message_remove_incoming(context, base_msg->data.source_mid);
+		cmsg_stored = NULL;
+	}
+
+	if(!cmsg_stored){
+		if(base_msg->data.qos > 0 && context->msgs_in.inflight_quota == 0){
+			/* Client isn't allowed any more incoming messages, so fail early */
+			db__msg_store_free(base_msg);
+			return MOSQ_ERR_RECEIVE_MAXIMUM_EXCEEDED;
+		}
+
+		if(base_msg->data.qos == 0
+				|| db__ready_for_flight(context, mosq_md_in, base_msg->data.qos)
+				){
+
+			dup = 0;
+			rc = db__message_store(context, base_msg, message_expiry_interval, mosq_mo_client);
+			if(rc){
+				return rc;
+			}
+		}else{
+			/* Client isn't allowed any more incoming messages, so fail early */
+			return process_bad_message(context, base_msg, MQTT_RC_QUOTA_EXCEEDED);
+		}
+		stored = base_msg;
+		base_msg = NULL;
+		dup = 0;
+	}else{
+		db__msg_store_free(base_msg);
+		base_msg = NULL;
+		stored = cmsg_stored->base_msg;
+		cmsg_stored->data.dup++;
+		dup = cmsg_stored->data.dup;
+	}
+
+	switch(stored->data.qos){
+		case 0:
+			rc2 = sub__messages_queue(context->id, stored->data.topic, stored->data.qos, stored->data.retain, &stored);
+			if(rc2 > 0){
+				rc = rc2;
+			}
+			break;
+		case 1:
+			util__decrement_receive_quota(context);
+			rc2 = sub__messages_queue(context->id, stored->data.topic, stored->data.qos, stored->data.retain, &stored);
+			/* stored may now be free, so don't refer to it */
+			if(rc2 == MOSQ_ERR_SUCCESS || context->protocol != mosq_p_mqtt5){
+				rc2 = send__puback(context, mid, 0, NULL);
+				if(rc2){
+					rc = rc2;
+				}
+			}else if(rc2 == MOSQ_ERR_NO_SUBSCRIBERS){
+				rc2 = send__puback(context, mid, MQTT_RC_NO_MATCHING_SUBSCRIBERS, NULL);
+				if(rc2){
+					rc = rc2;
+				}
+			}else{
+				rc = rc2;
+			}
+			break;
+		case 2:
+			{
+				int res;
+				if(dup == 0){
+					res = db__message_insert_incoming(context, 0, stored, true);
+				}else{
+					res = 0;
+				}
+
+				/* db__message_insert() returns 2 to indicate dropped message
+				 * due to queue. This isn't an error so don't disconnect them. */
+				/* FIXME - this is no longer necessary due to failing early above */
+				if(!res){
+					if(dup == 0 || dup == 1){
+						rc2 = send__pubrec(context, stored->data.source_mid, 0, NULL);
+						if(rc2){
+							rc = rc2;
+						}
+					}else{
+						return MOSQ_ERR_PROTOCOL;
+					}
+				}else{
+					rc = res;
+				}
+				break;
+			}
+	}
+
+	db__message_write_queued_in(context);
+	return rc;
+}
+
+
 int handle__publish(struct mosquitto *context)
 {
 	uint8_t dup;
 	int rc = 0;
-	int rc2;
 	uint8_t header = context->in_packet.command;
-	int res = 0;
-	struct mosquitto__base_msg *base_msg, *stored = NULL;
-	struct mosquitto__client_msg *cmsg_stored = NULL;
+	struct mosquitto__base_msg *base_msg;
 	size_t len;
 	uint16_t slen;
 	char *topic_mount;
 	mosquitto_property *properties = NULL;
 	uint32_t message_expiry_interval = MSG_EXPIRY_INFINITE;
 	int topic_alias = -1;
-	uint8_t reason_code = 0;
 	uint16_t mid = 0;
 
 	if(context->state != mosq_cs_active){
@@ -181,8 +340,7 @@ int handle__publish(struct mosquitto *context)
 	if(base_msg->data.payloadlen){
 		if(db.config->message_size_limit && base_msg->data.payloadlen > db.config->message_size_limit){
 			log__printf(NULL, MOSQ_LOG_DEBUG, "Dropped too large PUBLISH from %s (d%d, q%d, r%d, m%d, '%s', ... (%ld bytes))", context->id, dup, base_msg->data.qos, base_msg->data.retain, base_msg->data.source_mid, base_msg->data.topic, (long)base_msg->data.payloadlen);
-			reason_code = MQTT_RC_PACKET_TOO_LARGE;
-			goto process_bad_message;
+			return process_bad_message(context, base_msg, MQTT_RC_PACKET_TOO_LARGE);
 		}
 		base_msg->data.payload = mosquitto_malloc(base_msg->data.payloadlen+1);
 		if(base_msg->data.payload == NULL){
@@ -205,8 +363,7 @@ int handle__publish(struct mosquitto *context)
 				"Denied PUBLISH from %s (d%d, q%d, r%d, m%d, '%s', ... (%ld bytes))",
 				context->id, dup, base_msg->data.qos, base_msg->data.retain, base_msg->data.source_mid, base_msg->data.topic,
 				(long)base_msg->data.payloadlen);
-		reason_code = MQTT_RC_NOT_AUTHORIZED;
-		goto process_bad_message;
+		return process_bad_message(context, base_msg, MQTT_RC_NOT_AUTHORIZED);
 	}else if(rc != MOSQ_ERR_SUCCESS){
 		db__msg_store_free(base_msg);
 		return rc;
@@ -220,150 +377,9 @@ int handle__publish(struct mosquitto *context)
 		db__msg_store_free(base_msg);
 		return rc;
 #else
-		reason_code = MQTT_RC_IMPLEMENTATION_SPECIFIC;
-		goto process_bad_message;
+		return process_bad_message(context, base_msg, MQTT_RC_IMPLEMENTATION_SPECIFIC);
 #endif
 	}
 
-	{
-		rc = plugin__handle_message_in(context, &base_msg->data);
-		if(rc == MOSQ_ERR_ACL_DENIED){
-			log__printf(NULL, MOSQ_LOG_DEBUG,
-					"Denied PUBLISH from %s (d%d, q%d, r%d, m%d, '%s', ... (%ld bytes))",
-					context->id, dup, base_msg->data.qos, base_msg->data.retain, base_msg->data.source_mid, base_msg->data.topic,
-					(long)base_msg->data.payloadlen);
-
-			reason_code = MQTT_RC_NOT_AUTHORIZED;
-			goto process_bad_message;
-		}else if(rc == MOSQ_ERR_QUOTA_EXCEEDED){
-			log__printf(NULL, MOSQ_LOG_DEBUG,
-					"Rejected PUBLISH from %s, quota exceeded.", context->id);
-
-			reason_code = MQTT_RC_QUOTA_EXCEEDED;
-			goto process_bad_message;
-		}else if(rc != MOSQ_ERR_SUCCESS){
-			db__msg_store_free(base_msg);
-			return rc;
-		}
-	}
-
-	if(base_msg->data.qos > 0){
-		db__message_store_find(context, base_msg->data.source_mid, &cmsg_stored);
-	}
-
-	if(cmsg_stored && base_msg->data.source_mid != 0 &&
-			(cmsg_stored->base_msg->data.qos != base_msg->data.qos
-			|| cmsg_stored->base_msg->data.payloadlen != base_msg->data.payloadlen
-			|| strcmp(cmsg_stored->base_msg->data.topic, base_msg->data.topic)
-			|| memcmp(cmsg_stored->base_msg->data.payload, base_msg->data.payload, base_msg->data.payloadlen))){
-
-		log__printf(NULL, MOSQ_LOG_WARNING, "Reused message ID %u from %s detected. Clearing from storage.", base_msg->data.source_mid, context->id);
-		db__message_remove_incoming(context, base_msg->data.source_mid);
-		cmsg_stored = NULL;
-	}
-
-	if(!cmsg_stored){
-		if(base_msg->data.qos > 0 && context->msgs_in.inflight_quota == 0){
-			/* Client isn't allowed any more incoming messages, so fail early */
-			db__msg_store_free(base_msg);
-			return MOSQ_ERR_RECEIVE_MAXIMUM_EXCEEDED;
-		}
-
-		if(base_msg->data.qos == 0
-				|| db__ready_for_flight(context, mosq_md_in, base_msg->data.qos)
-				){
-
-			dup = 0;
-			rc = db__message_store(context, base_msg, &message_expiry_interval, mosq_mo_client);
-			if(rc){
-				return rc;
-			}
-		}else{
-			/* Client isn't allowed any more incoming messages, so fail early */
-			reason_code = MQTT_RC_QUOTA_EXCEEDED;
-			goto process_bad_message;
-		}
-		stored = base_msg;
-		base_msg = NULL;
-		dup = 0;
-	}else{
-		db__msg_store_free(base_msg);
-		base_msg = NULL;
-		stored = cmsg_stored->base_msg;
-		cmsg_stored->data.dup++;
-		dup = cmsg_stored->data.dup;
-	}
-
-	switch(stored->data.qos){
-		case 0:
-			rc2 = sub__messages_queue(context->id, stored->data.topic, stored->data.qos, stored->data.retain, &stored);
-			if(rc2 > 0){
-				rc = rc2;
-			}
-			break;
-		case 1:
-			util__decrement_receive_quota(context);
-			rc2 = sub__messages_queue(context->id, stored->data.topic, stored->data.qos, stored->data.retain, &stored);
-			/* stored may now be free, so don't refer to it */
-			if(rc2 == MOSQ_ERR_SUCCESS || context->protocol != mosq_p_mqtt5){
-				rc2 = send__puback(context, mid, 0, NULL);
-				if(rc2){
-					rc = rc2;
-				}
-			}else if(rc2 == MOSQ_ERR_NO_SUBSCRIBERS){
-				rc2 = send__puback(context, mid, MQTT_RC_NO_MATCHING_SUBSCRIBERS, NULL);
-				if(rc2){
-					rc = rc2;
-				}
-			}else{
-				rc = rc2;
-			}
-			break;
-		case 2:
-			if(dup == 0){
-				res = db__message_insert_incoming(context, 0, stored, true);
-			}else{
-				res = 0;
-			}
-
-			/* db__message_insert() returns 2 to indicate dropped message
-			 * due to queue. This isn't an error so don't disconnect them. */
-			/* FIXME - this is no longer necessary due to failing early above */
-			if(!res){
-				if(dup == 0 || dup == 1){
-					rc2 = send__pubrec(context, stored->data.source_mid, 0, NULL);
-					if(rc2){
-						rc = rc2;
-					}
-				}else{
-					return MOSQ_ERR_PROTOCOL;
-				}
-			}else{
-				rc = res;
-			}
-			break;
-	}
-
-	db__message_write_queued_in(context);
-	return rc;
-process_bad_message:
-	rc = MOSQ_ERR_UNKNOWN;
-	if(base_msg){
-		switch(base_msg->data.qos){
-			case 0:
-				rc = MOSQ_ERR_SUCCESS;
-				break;
-			case 1:
-				rc = send__puback(context, base_msg->data.source_mid, reason_code, NULL);
-				break;
-			case 2:
-				rc = send__pubrec(context, base_msg->data.source_mid, reason_code, NULL);
-				break;
-		}
-		db__msg_store_free(base_msg);
-	}
-	if(db.config->max_queued_messages > 0 && context->out_packet_count >= db.config->max_queued_messages){
-		rc = MQTT_RC_QUOTA_EXCEEDED;
-	}
-	return rc;
+	return handle__accepted_publish(context, base_msg, mid, dup, &message_expiry_interval);
 }
