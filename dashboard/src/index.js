4cfab69b9471da5c7787e3261010ba8a16ea9c20
Serhii Orlivskyi <serhii.orlivskyi@cedalo.com>
2025-11-12 00:26:05 +0100

    Dashboard: add a banner
    Signed-off-by: Serhii Orlivskyi <serhii.orlivskyi@cedalo.com> (Cedalo GmbH)

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -3,10 +3,55 @@ document.addEventListener("DOMContentLoaded", () => {
   new MosquittoDashboard();
 });
 
+function checkNormalBannerImage(bannerImage, bannerCard) {
+  const imageSrc = "https://mosquitto.org/banner"; // no extension on the file - it can svg or png
+  const probe = new Image();
+  probe.onload = () => {
+    bannerImage.src = imageSrc;
+    bannerCard.classList.remove("hidden");
+  };
+  probe.onerror = () => {
+    console.warn("Banner-image didn't loaded");
+  };
+  probe.src = imageSrc;
+}
+
+function checkSvgBannerImage(bannerImage, bannerCard, bannerLink, bannerInner) {
+  // if a full fledged svg found, display it and remove the default link
+  const svgSrc = "https://mosquitto.org/banner.svg";
+  const svgProbe = new Image();
+  svgProbe.onload = () => {
+    bannerImage.src = svgSrc;
+    // Only if the svg exists (was loaded successfully) make a call to fetch it and then inline it. Requires CORS to be set on svgSrc
+    fetch(svgSrc)
+      .then((r) => r.text())
+      .then((svg) => {
+        bannerInner.innerHTML = svg;
+        bannerCard.classList.remove("hidden");
+        bannerLink.removeAttribute("href");
+      })
+      .catch((error) =>
+        console.warn("SVG banner-image couldn't be fetched:", error),
+      );
+  };
+  svgProbe.onerror = () => {
+    console.warn("SVG Banner-image didn't loaded");
+  };
+  svgProbe.src = svgSrc;
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   const toggleButton = document.getElementById("layout-toggle");
   const chartsGrid = document.getElementById("charts-grid");
   const layoutText = document.getElementById("layout-text");
+  const bannerImage = document.getElementById("banner-img");
+  const bannerCard = document.getElementById("banner-card");
+  const bannerInner = document.getElementById("banner-inner");
+  const bannerLink = document.getElementById("banner-link");
+
+  checkNormalBannerImage(bannerImage, bannerCard);
+
+  checkSvgBannerImage(bannerImage, bannerCard, bannerLink, bannerInner);
 
   let isGridView = true;
   let storedSetting = sessionStorage.getItem("isGridView");
