3093333217607c36551e78d027903d253eba2046
Roger A. Light <roger@atchoo.org>
2026-01-20 10:15:17 +0000

    Remove ENGINE_cleanup as it does nothing any more.
    Closes #3450. Thanks to Paolo Patruno

--- lib/net_mosq.c
+++ lib/net_mosq.c
@@ -160,9 +160,6 @@ void net__cleanup(void)
 #ifdef WITH_TLS
 	cleanup_ui_method();
 	if(is_tls_initialized){
-#  if !defined(OPENSSL_NO_ENGINE) && OPENSSL_API_LEVEL < 30000
-		ENGINE_cleanup();
-#  endif
 		is_tls_initialized = false;
 	}
 #endif
